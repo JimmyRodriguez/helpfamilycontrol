8f06fd7c337fe2066e82b02b4cc2303c0a530f13
jimmyrodriguez <jimmy_sidney@hotmail.es>
2016-11-16 02:07:26 -0600

    agregar ruta public_html

--- Vista/layout/vistas/admon/iEmpleados.php
+++ Vista/layout/vistas/admon/iEmpleados.php
@@ -1,8 +1,8 @@
 <?php
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/Modelo/BASE_DE_DATOS.php');
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/Controlador/ESTADO.php');
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/Controlador/SEXO.php');
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/Controlador/EMPLEADO.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/public_html/Modelo/BASE_DE_DATOS.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/public_html/Controlador/ESTADO.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/public_html/Controlador/SEXO.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) .'/public_html/Controlador/EMPLEADO.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
